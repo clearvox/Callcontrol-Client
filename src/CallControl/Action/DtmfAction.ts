10ed01870abf5547bfbb2dd3d5f859a3509b73d4
Joris <joris.vanbentum@x2com.nl>
2020-07-22 11:49:17 +0200

    18 Added number as a type for digit

--- src/CallControl/Action/DtmfAction.ts
+++ src/CallControl/Action/DtmfAction.ts
@@ -1,7 +1,7 @@
 import {ModifyCallAction} from "./ModifyCallAction";
 
 export class DtmfAction extends ModifyCallAction {
-    constructor(callID: string, digit: string, phoneID?: string) {
+    constructor(callID: string, digit: string|number, phoneID?: string) {
         super(callID, 'dtmf', {
             digit: digit,
             phone_id: phoneID
